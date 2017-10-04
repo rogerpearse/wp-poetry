61465bef048bbe8f1b39f0be3bd11956dc3a6e1d
Roger Pearse <rpearse@domain.whiteclarkegroup.com>
2017-10-04 09:48:34 +0100

    tweaks

--- wp-poetry.php
+++ wp-poetry.php
@@ -75,3 +75,67 @@ function run_wp_poetry() {
 
 }
 run_wp_poetry();
+
+/** =================== The plugin stuff for functions.php ======================= */
+
+/**
+	Add a menu with styles in to the second row of the editor (the "kitchen sink")
+	This just picks up ordinary styles by default.
+	https://torquemag.io/2016/09/add-custom-styles-wordpress-editor-manually-via-plugin/
+ */
+function add_style_select_buttons( $buttons ) {
+    array_unshift( $buttons, 'styleselect' );
+    return $buttons;
+}
+
+// Register our callback to the appropriate filter
+add_filter( 'mce_buttons_2', 'add_style_select_buttons' );
+
+/** 
+	Add custom styles to the WordPress editor
+	This replaces the default styles with these custom styles.
+	In the code snippet above, we are creating three different 
+	custom styles. In particular, we are adding options to 
+	create a red button, a content box, and a highlighter for text passages.
+  */
+function my_custom_styles( $init_array ) {  
+ 
+    $style_formats = array(  
+        // These are the custom styles
+        array(  
+            'title' => 'Red Button',  
+            'block' => 'span',  
+            'classes' => 'red-button',
+            'wrapper' => true,
+        ),  
+        array(  
+            'title' => 'Content Block',  
+            'block' => 'span',  
+            'classes' => 'content-block',
+            'wrapper' => true,
+        ),
+        array(  
+            'title' => 'Highlighter',  
+            'block' => 'span',  
+            'classes' => 'highlighter',
+            'wrapper' => true,
+        ),
+    );  
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );  
+    
+    return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'my_custom_styles' );
+
+// Enable styles in the editor as well as display.  Path is relative to the active theme root
+function custom_editor_styles() {
+	add_editor_style( '../../plugins/wp-poetry/wp-poetry-styles.css' );
+}
+ 
+add_action( 'init', 'custom_editor_styles' );
+
+
+
